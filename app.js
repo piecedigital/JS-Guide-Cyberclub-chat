c114182fa5093d54fcc8527277d66fbff472c265
Darryl Benton Dixon <piecedigitalstudios@gmail.com>
2015-09-17 21:44:39 -0400

    added views set line

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var priVar = require("./modules/private-variables");
 
 // config
 // view engine setup
-//app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.set("view options", { layout: "layout" });
 
