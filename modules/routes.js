2d52a142278804c694c2248891303f0d2e4683c2
Darryl Benton Dixon <piecedigitalstudios@gmail.com>
2015-12-03 14:44:15 -0500

    improved ntifications

--- modules/routes.js
+++ modules/routes.js
@@ -84,11 +84,15 @@ sass.render({
 					if(chatQErr) throw chatQErr;
 
 					if(!chatQDoc) {
+						console.log(req.headers.host)
+						res.setHeader("X-Frame-Options", ["ALLOW-FROM", `http://${req.headers.host}`]);
 						next();
 					} else {
 						if(!req.originalUrl.match(/\/banned\/ip/)) {
 							res.redirect("banned/ip");
 						} else {
+							console.log(req.headers.host)
+							res.setHeader("X-Frame-Options", ["ALLOW-FROM", `http://${req.headers.host}`]);
 							next();
 						}
 					}
